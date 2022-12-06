b931eff517efbdca8d2897465848cb1e4aebea70
Bruce Luo <103285719+BruceLuooo@users.noreply.github.com>
2022-12-06 02:37:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ connectDB();
 
 //middleware
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
