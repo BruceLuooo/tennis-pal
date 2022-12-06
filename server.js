0ade6863463c438fd9bce82a48ad2da8d93a20cc
Bruce Luo <103285719+BruceLuooo@users.noreply.github.com>
2022-12-06 02:51:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,12 +17,7 @@ connectDB();
 //middleware
 app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname + '/client/build/index.html'));
-	});
-}
+app.use(express.static('client/build'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -34,6 +29,12 @@ app.use('/api/messages', require('./routes/allMessagesRoutes'));
 
 app.use(handleError);
 app.use(notFoundMiddleware);
+
+if (process.env.NODE_ENV === 'production') {
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname + '/client/build/index.html'));
+	});
+}
 
 const server = app.listen(PORT, () =>
 	console.log(`server started on port ${PORT}`),
