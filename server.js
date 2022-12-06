20897a53f25b3d3254087d6dc6dd487e6585845c
Bruce Luo <103285719+BruceLuooo@users.noreply.github.com>
2022-12-06 02:24:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.use(notFoundMiddleware);
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
-	app.get('/*', function (req, res) {
-		res.sendFile(path.join(__dirname, './client/build/index.html'));
+	app.get('*', function (req, res) {
+		res.sendFile(path.resolve(__dirname, './client/build/index.html'));
 	});
 }
 
